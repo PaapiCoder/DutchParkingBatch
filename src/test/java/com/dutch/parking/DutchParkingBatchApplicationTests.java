44161ca756d5b567ea2bbb15280f5c21c899b6dd
jatinpandey <jatinder.pandey@gmail.com>
2023-11-28 22:56:02 +0530

    Removed old classes for DB and implemented new way using Rest Template to call the Api to fetch data details.

--- src/test/java/com/dutch/parking/DutchParkingBatchApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.dutch.parking;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DutchParkingBatchApplicationTests {
-
-}
